9a26211642532ef454f1ba479ffdc50984946703
erikc96 <craigoej@gmail.com>
2021-05-14 09:53:18 -0600

    build

--- src/pages/pp.tsx
+++ src/pages/pp.tsx
@@ -1,72 +1,8 @@
 import React, { useState, useCallback } from "react";
 import Layout from "../layouts";
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const PP = () => {
-  const [numPages, setNumPages] = useState<number>(0);
-  const [pageNumber, setPageNumber] = useState(1);
-  const [ref, setRef] = useState<any>(React.createRef());
-
-  const PrivacyPolicyPDF = require("../pdf/pp.pdf");
-
-  const onRefChange = useCallback((node) => {
-    // ref value changed to node
-    setRef(node); // e.g. change ref state to trigger re-render
-    ref.current.style.margin = "auto auto";
-    if (node === null) {
-      // node is null, if DOM node of ref had been unmounted before
-    } else {
-      // ref value exists
-    }
-  }, []);
-
-  function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
-    setNumPages(numPages);
-    setPageNumber(1);
-  }
-
-  function changePage(offset: number) {
-    setPageNumber((prevPageNumber) => prevPageNumber + offset);
-  }
-
-  function previousPage() {
-    changePage(-1);
-  }
-
-  function nextPage() {
-    changePage(1);
-  }
-
-  return (
-    <Layout>
-      <div className="mt-32 mb-8 grid grid-cols-3">
-        <button type="button" disabled={pageNumber <= 1} onClick={previousPage}>
-          Previous
-        </button>
-        <div className="mx-auto text-center">
-          Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-        </div>
-        <button
-          type="button"
-          disabled={pageNumber >= numPages}
-          onClick={nextPage}
-        >
-          Next
-        </button>
-      </div>
-      <div className="mx-auto" style={{ width: "38rem" }}>
-        <Document
-          file={PrivacyPolicyPDF}
-          className="mx-auto"
-          onLoadSuccess={onDocumentLoadSuccess}
-          canvasRef={ref}
-        >
-          <Page className={"mx-auto"} pageNumber={pageNumber} />
-        </Document>
-      </div>
-    </Layout>
-  );
+  return <div></div>;
 };
 
 export default PP;
