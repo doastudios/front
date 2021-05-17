48278eaec0fb9a2ae5563f72f8246a607200dbee
erikc96 <craigoej@gmail.com>
2021-05-16 20:27:57 -0600

    privacy policy

--- src/pages/pp.tsx
+++ src/pages/pp.tsx
@@ -5,7 +5,7 @@ const PP = () => {
   return (
     <Layout>
       <div className="w-full h-screen">
-        <iframe src={"/pp.html"} className="w-full h-full"></iframe>
+        <iframe src={"/privacy_policy.html"} className="w-full h-full"></iframe>
       </div>
     </Layout>
   );
