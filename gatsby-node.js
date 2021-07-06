50bd1a2b2dbe86bf96c04835587f0c65366c8271
erikc96 <craigoej@gmail.com>
2021-07-05 20:24:45 -0400

    Fix build error related to large images  by disabling some obscure setting

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,32 @@
 const path = require(`path`);
+const sharp = require("sharp");
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-  return graphql(`
-    {
-      allShopifyProduct {
-        edges {
-          node {
-            handle
-          }
-        }
-      }
-    }
-  `).then((result) => {
-    result.data.allShopifyProduct.edges.forEach(({ node }) => {
-      createPage({
-        path: `/product/${node.handle}/`,
-        component: path.resolve(`./src/components/ProductPage/index.tsx`),
-        context: {
-          // Data passed to context is available
-          // in page queries as GraphQL variables.
-          handle: node.handle,
-        },
-      });
-    });
-  });
-};
+sharp.cache(false);
+sharp.simd(false);
+
+//exports.createPages = ({ graphql, actions }) => {
+//  const { createPage } = actions;
+//  return graphql(`
+//    {
+//      allShopifyProduct {
+//        edges {
+//          node {
+//            handle
+//          }
+//        }
+//      }
+//    }
+//  `).then((result) => {
+//    result.data.allShopifyProduct.edges.forEach(({ node }) => {
+//      createPage({
+//        path: `/product/${node.handle}/`,
+//        component: path.resolve(`./src/components/ProductPage/index.tsx`),
+//        context: {
+//          // Data passed to context is available
+//          // in page queries as GraphQL variables.
+//          handle: node.handle,
+//        },
+//      });
+//    });
+//  });
+//};
